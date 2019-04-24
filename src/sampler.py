2513d3291ec5dd6531b77827e0f98477e32fbdd8
KariEriksen <eriksen_kari@hotmail.com>
2019-04-24 18:21:53 +0200

    added folders

---
+++ src/sampler.py
@@ -0,0 +1,20 @@
+import numpy as np
+import sys
+from system import System
+
+class Sampler(System):
+
+	"""
+
+	"""
+
+	def local_energy():
+
+
+	def energy_gradient():
+
+
+	def probability():
+
+
+	def drift_force():
