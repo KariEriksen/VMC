0a86b1559074ffc4b64ce60cc661580ac636ee99
KariEriksen <eriksen_kari@hotmail.com>
2019-04-29 18:24:13 +0200

    working on metropolis

--- src/metropolis.py
+++ src/metropolis.py
@@ -1,12 +1,27 @@
 import numpy as np
 import sys
 from system import System
+from sampler import Sampler
 
 class Metropolis(System):
 
-	def __init__(self, positions):
+	def __init__(self, delta_R, delta_t):
 
-		self.positions = positions
+		self.delta_R = delta_R
+
+		System.__init__(self, num_particles, num_dimensions, monte_carlo_cycles, 
+		variational_parameters, step_length)
+
+		self.num_particles          = num_p
+		self.num_dimensionsn        = num_d
+		self.monte_carlo_cycles     = mcc
+		self.variational_parameters = vari_p
+		self.step_length            = step
+
+		alpha     = vari_p[0]
+		beta      = vari_p[1]
+		omega     = vari_p[3]
+		a         = vari_p[4]
 
 
 	def new_positions(self):
@@ -17,8 +32,8 @@ class Metropolis(System):
 		in the spatial configuration space
 		"""
 
-		r = np.random.sample()
-		new_positions = positions + r*delta_R
+		r = np.random.rand(num_p, num_d)
+		new_positions = positions + np.multiply(r, delta_R)
 
 
 	def metropolis(self):
@@ -27,6 +42,17 @@ class Metropolis(System):
 		Running the naive metropolis algorithm with brute-force sampling of
 		new positions
 		"""
+
+		acceptance_ratio = Sampler.probability()
+		epsilon = np.random.sampler()
+
+		if acceptance_ratio < epsilon:
+			positions = new_positions
+
+		else:
+			pass
+
+		Sampler.local_energy(positions)
 
 
 	def importance_sampling(self):
@@ -37,6 +63,13 @@ class Metropolis(System):
 		D is the diffusion coefficient equal 0.5, xi is a gaussion random variable
 		and delta_t is the time step between 0.001 and 0.01
 		"""
+
+		D = 0.5
+		F = Sampler.drift_force
+		xi = np.random.sampler()
+		new_positions_importance = positions + D*F*delta_t + xi*sqrt(delta_t)
+
+		
 
 	def gibbs_sampling(self):
 	
