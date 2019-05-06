791173f0fce1b8f8f17221a85557993426882537
KariEriksen <eriksen_kari@hotmail.com>
2019-05-06 12:25:08 +0200

    small changes

--- run_vmc.py
+++ run_vmc.py
@@ -16,12 +16,13 @@ monte_carlo_cycles       = 5
 number_of_particles      = 3
 number_of_dimensions     = 3
 numerical_step_length    = 0.1
+step_metropolis          = 0.1
+step_importance          = 0.1
 alpha                    = 0.1
 beta                     = 1.0
 a                        = 0.0
 omega                    = 0.01
-step_metropolis          = 0.1
-step_importance          = 0.1
+
 positions = np.random.rand(number_of_particles, number_of_dimensions)
 
 System(number_of_particles, number_of_dimensions, positions, 
@@ -29,12 +30,14 @@ System(number_of_particles, number_of_dimensions, positions,
 
 Sampler(omega, numerical_step_length)
 
+m = Metropolis(step_metropolis, step_importance, number_of_particles, 
+			   number_of_dimensions, positions, step_metropolis)
+
 for i in range(monte_carlo_cycles):
 
 	"""
 	Run the metropolis algo for given Monte Carlo cycles.
 	"""
+	m.metropolis()
 
-	m = Metropolis(step_metropolis, step_importance, number_of_particles, 
-				   number_of_dimensions, positions, step_metropolis)
 
