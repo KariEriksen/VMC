d1893af672f6f54a1e822c72ad9e71f1ffbe5f24
KariEriksen <eriksen_kari@hotmail.com>
2019-05-22 17:06:05 +0200

    changes

--- src/optimizer.py
+++ src/optimizer.py
@@ -1,21 +1,28 @@
 import numpy as np
 
 from sampler import Sampler
+from metropolis import Metropolis
+from system import System
 
 class Optimizer:
 
-	#S = Sampler(omega, step)
-
-	def __init__(self, learning_rate, sampler):
+	def __init__(self, learning_rate, iterations, sampler, metropolis):
 
 		self.learning_rate = learning_rate
+		self.iterations    = iterations
 		self.s             = sampler
+		self.m             = metropolis
 
-	def gradient_descent(self, alpha, posistions):
+	def gradient_descent(self):
 
-		der_energy = self.s.energy_gradient(posistions)
-		new_alpha  = alpha - self.learning_rate*der_energy
+		for i in range(self.iterations):
 
-		return new_alpha
+			der_energy = self.s.energy_gradient(posistions)
+			new_alpha  = alpha - self.learning_rate*der_energy
+			System.update(alpha)
+
+			new_energy = m.metropolis() 
+
+		return 0
 
 
