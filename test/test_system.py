316f1ea98526ec9d813cd55b1c9d220bec4bfa25
Morten Ledum <morten.ledum@gmail.com>
2019-06-27 15:39:05 +0200

    Add tests with alpha /= 1.0 for single_particle_function

--- test/test_system.py
+++ test/test_system.py
@@ -7,7 +7,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir, 'src'))
 from system import System  # noqa: 401
 
 
-def test_system_wavefunction():
+def test_system_wavefunction_2d():
     num_particles = 1
     num_dimensions = 2
     alpha = 1.0
@@ -16,12 +16,22 @@ def test_system_wavefunction():
 
     s = System(num_particles, num_dimensions, alpha, beta, a)
 
-    positions = np.random.rand(num_particles, num_dimensions)
+    positions = np.zeros(shape=(num_particles, num_dimensions))
     x = 2.92858925782756
     y = 0.00925285752985
     positions[0, 0] = x
     positions[0, 1] = y
 
     wf = s.single_particle_function(positions)
-
     assert wf == pytest.approx(np.exp(-x**2 - y**2), abs=1e-14)
+
+    for _ in range(50):
+        alpha = np.random.uniform(1e-3, 10)
+        s.alpha = alpha
+
+        x = np.random.uniform(-20, 20)
+        y = np.random.uniform(-20, 20)
+        positions[0, 0] = x
+        positions[0, 1] = y
+        wf = s.single_particle_function(positions)
+        assert wf == pytest.approx(np.exp(-alpha*(x**2 + y**2)), abs=1e-14)
