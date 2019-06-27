4282bbd41b4c9d16a0b8acc31b79def56f195517
KariEriksen <eriksen_kari@hotmail.com>
2019-06-27 16:38:47 +0200

    Add empty test files for metropolis, sampler, optimizer

---
+++ test/test_metropolis.py
@@ -0,0 +1,7 @@
+import pytest
+import numpy as np
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir, 'src'))
+from metropolis import Metropolis  # noqa: 401
