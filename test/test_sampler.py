47b7aac08fe21201ba01acee9645453b2e83c9fe
KariEriksen <eriksen_kari@hotmail.com>
2019-06-28 17:38:24 +0200

    added test on potential energy in sampler

--- test/test_sampler.py
+++ test/test_sampler.py
@@ -66,7 +66,7 @@ def test_kinetic_energy_3d():
         z = np.random.uniform(-20, 20)
         positions[0, 0] = x
         positions[0, 1] = y
-        positions[0, 2] = z*beta
+        positions[0, 2] = z
         pos_xp = np.array(positions)
         pos_xn = np.array(positions)
         pos_yp = np.array(positions)
@@ -94,7 +94,32 @@ def test_kinetic_energy_3d():
 
 def test_potential_energy():
 
-    assert 1 == 1
+    num_particles = 1
+    num_dimensions = 3
+    numerical_step = 0.001
+    a = 0.0
+    positions = np.zeros(shape=(num_particles, num_dimensions))
+    for _ in range(50):
+        alpha = np.random.uniform(1e-3, 10)
+        beta = np.random.uniform(1e-3, 10)
+        omega = np.random.uniform(1e-3, 10)
+        sys = System(num_particles, num_dimensions, alpha, beta, a)
+        sam = Sampler(omega, numerical_step, sys)
+
+        x = np.random.uniform(-20, 20)
+        y = np.random.uniform(-20, 20)
+        z = np.random.uniform(-20, 20)
+        positions[0, 0] = x
+        positions[0, 1] = y
+        positions[0, 2] = z*beta
+
+        sum = 0.0
+        for i in range(num_dimensions):
+            sum += (positions[0, i]*positions[0, i])
+
+        pot_energy = 0.5*omega*omega*(sum)
+        assert pot_energy == pytest.approx(sam.potential_energy(positions),
+                                           abs=1e-14)
 
 
 def test_local_energy():
