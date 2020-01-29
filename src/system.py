16752074e5324eaebbbd594dad46b112e562ccc6
KariEriksen <eriksen_kari@hotmail.com>
2020-01-29 16:38:29 +0100

    position as input to functions, not object

--- src/system.py
+++ src/system.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 class System:
-    """Contains position matrix."""
+    """Contains distance matrix."""
 
     # deri_psi = 0.0
     # g        = 0.0
@@ -14,22 +14,21 @@ class System:
         """Instance of class."""
         self.num_p = num_particles
         self.num_d = num_dimensions
-        self.positions = np.random.rand(self.num_p, self.num_d)
         self.r_squared = np.zeros(self.num_p)
         self.distances = np.zeros((self.num_p, self.num_p))
 
-    def positions_squared(self):
+    def positions_squared(self, positions):
         """Calculate the distance from origo"""
 
         for i in range(self.num_p):
-            x = self.positions[i, 0]
-            y = self.positions[i, 1]
-            z = self.positions[i, 2]
+            x = positions[i, 0]
+            y = positions[i, 1]
+            z = positions[i, 2]
             self.r_squared[i] = x*x + y*y + z*z
 
         return self.r_squared
 
-    def positions_distances(self):
+    def positions_distances(self, positions):
         """Calculate the distances between particles"""
 
         for i in range(self.num_p):
@@ -37,22 +36,22 @@ class System:
                 # ri_minus_rj = np.subtract(positions[i, :], positions[j+1, :])
                 r = 0.0
                 for k in range(self.num_d):
-                    ri_minus_rj = (self.positions[i, k] -
-                                   self.positions[j+1, k])
+                    ri_minus_rj = (positions[i, k] -
+                                   positions[j+1, k])
                     r += ri_minus_rj**2
                 self.distances[i, j+1] = math.sqrt(r)
 
         return self.distances
 
-    def positions_update(self, new_position, i):
+    def positions_update(self, positions, new_position, i):
         """Update the position matrix for movement of one particle"""
         """particle i = particle number"""
 
-        self.positions[i, :] = new_position
+        positions[i, :] = new_position
 
-        return self.positions
+        return positions
 
-    def distances_update(self, i):
+    def distances_update(self, positions, i):
         """Update the distance matrix for movement of one particle"""
         """particle i = particle index"""
 
@@ -60,7 +59,7 @@ class System:
             if j != i:
                 r = 0.0
                 for k in range(self.num_d):
-                    ri_minus_rj = self.positions[i, k] - self.positions[j, k]
+                    ri_minus_rj = positions[i, k] - positions[j, k]
                     r += ri_minus_rj**2
                 self.distances[i, j] = math.sqrt(r)
                 self.distances[j, i] = self.distances[i, j]
