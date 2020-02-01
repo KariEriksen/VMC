98f0ac7c715f8c82978a3e9b5666a72419e18244
KariEriksen <eriksen_kari@hotmail.com>
2020-02-01 18:59:57 +0100

    added a new case

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from vmc import non_interaction_case # noqa: 401
 from vmc import weak_interaction_case # noqa: 401
+from vmc import strong_interaction_case # noqa: 401
 from vmc import elliptic_weak_interaction_case # noqa: 401
 from vmc import brute_force # noqa: 401
 from vmc import one_body_density # noqa: 401
@@ -9,8 +10,9 @@ from vmc import run_blocking # noqa: 401
 """case(monte_carlo_cycles, number of particles,
         number of dimensions, interaction parameter)"""
 
-non_interaction_case(int(10e4), 1, 3, 0.4)
-# weak_interaction_case(100000, 2, 3, 0.47)
+# non_interaction_case(int(10e4), 1, 3, 0.4)
+# weak_interaction_case(100000, 2, 3, 0.4)
+strong_interaction_case(100000, 3, 3, 0.4)
 # elliptic_weak_interaction_case(10000, 2, 3, None)
 # brute_force(100000, 1, 3, None)
 # one_body_density(100000, 3, 3, None)
--- vmc.py
+++ vmc.py
@@ -11,7 +11,9 @@ from metropolis import Metropolis # noqa: 401
 from optimizer import Optimizer # noqa: 401
 from Hamiltonian.non_interaction import Non_Interaction # noqa: 401
 from Hamiltonian.weak_interaction import Weak_Interaction # noqa: 401
+from Hamiltonian.lennard_jones import Lennard_Jones # noqa: 401
 from Wavefunction.wavefunction import Wavefunction # noqa: 401
+from Wavefunction.mcmillian import McMillian_Wavefunction # noqa: 401
 from sampler import Sampler # noqa: 401
 from system import System # noqa: 401
 
@@ -115,14 +117,14 @@ def weak_interaction_case(monte_carlo_cycles, num_particles, num_dimensions,
         wave = Wavefunction(num_particles, num_dimensions, parameter,
                             beta, a, sys)
         # Run with analytical expression of local energy = true
-        hamilton = Non_Interaction(omega, wave, sys, 'true')
+        hamilton = Weak_Interaction(omega, wave, sys, 'true')
         met = Metropolis(monte_carlo_cycles, step_metropolis,
                          step_importance, num_particles, num_dimensions,
                          wave, hamilton, sys)
 
-        # d_El, energy = met.run_metropolis()
+        d_El, energy, var = met.run_metropolis()
         # Run with analytical expression for quantum force = true
-        d_El, energy, var = met.run_importance_sampling('true')
+        # d_El, energy, var = met.run_importance_sampling('true')
         stop = timeit.default_timer()
         new_parameter = opt.gradient_descent(parameter, d_El)
         # new_parameter = opt.gradient_descent_barzilai_borwein(parameter,
@@ -139,6 +141,63 @@ def weak_interaction_case(monte_carlo_cycles, num_particles, num_dimensions,
 
     print ('final run time: ', stop - start)
     with open('/home/kari/VMC/data/weak_interaction_case_data.csv', 'w',
+              newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["alpha", "derivative_energy", "local_energy",
+                        "variance"])
+        for i in range(len(d_El_array)):
+            writer.writerow([parameter_array[i], d_El_array[i],
+                            energy_array[i], var_array[i]])
+
+
+def strong_interaction_case(monte_carlo_cycles, num_particles, num_dimensions,
+                            alpha):
+    """Run the variational monte carlo."""
+
+    if alpha is None:
+        alpha = 0.48
+
+    epsilon = 1.0
+    sigma = 1.0
+
+    d_El_array = np.zeros(gradient_iterations)
+    energy_array = np.zeros(gradient_iterations)
+    parameter_array = np.zeros(gradient_iterations)
+    var_array = np.zeros(gradient_iterations)
+
+    parameter = alpha
+    start = timeit.default_timer()
+    sys = System(num_particles, num_dimensions)
+    for i in range(gradient_iterations):
+
+        # Call wavefunction class in order to set new alpha parameter
+        wave = McMillian_Wavefunction(num_particles, num_dimensions, parameter,
+                                      sys)
+        # Run with analytical expression of local energy = true
+        hamilton = Lennard_Jones(epsilon, sigma, wave, sys, 'true')
+        met = Metropolis(monte_carlo_cycles, step_metropolis,
+                         step_importance, num_particles, num_dimensions,
+                         wave, hamilton, sys)
+
+        d_El, energy, var = met.run_metropolis()
+        # Run with analytical expression for quantum force = true
+        # d_El, energy, var = met.run_importance_sampling('true')
+        stop = timeit.default_timer()
+        new_parameter = opt.gradient_descent(parameter, d_El)
+        # new_parameter = opt.gradient_descent_barzilai_borwein(parameter,
+        #                                                       d_El, i)
+        print ('new alpha = ', new_parameter)
+        print ('number of gradien descent runs = ', i)
+        print ('run time: ', stop - start)
+
+        d_El_array[i] = d_El
+        energy_array[i] = energy
+        var_array[i] = var
+        parameter_array[i] = new_parameter
+        parameter = new_parameter
+
+    print ('final run time: ', stop - start)
+    with open('/home/kari/VMC/data/strong_interaction_case_data.csv', 'w',
               newline='') as file:
         writer = csv.writer(file)
         writer.writerow(["alpha", "derivative_energy", "local_energy",
