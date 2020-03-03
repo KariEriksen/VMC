bed19421ceef6203d089b67a657ca3290740300a
KariEriksen <eriksen_kari@hotmail.com>
2020-03-03 16:39:56 +0100

    added extra case

--- plot/one_body_plot.py
+++ plot/one_body_plot.py
@@ -1,22 +1,25 @@
-import numpy as np 
+import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy.optimize import curve_fit
 from math import pi
 
 sns.set_style(style='white')
 
-data1 = pd.read_csv("../data/obd_int_data.csv")
-data2 = pd.read_csv("../data/obd_non_data.csv")
+data1 = pd.read_csv("../data/obd_non_data.csv")
+data2 = pd.read_csv("../data/obd_0043_data.csv")
+data3 = pd.read_csv("../data/obd_433_data.csv")
 
 
 def func(x, a, b, c):
     return a*np.exp(-(x-b)*(x-b)/(2*c*c))
 
+
 x = data1['r']
-y_int = data1['density']
-y_non = data2['density']
+y_non = data1['density']
+y_043 = data2['density']
+y_433 = data3['density']
 
 x = np.array(x)
 
@@ -28,21 +31,24 @@ for i in range(1, len(x)-1):
 
 V[40] = V[39]
 
-y_int = y_int
-y_non = y_non
-# fit the interactive case
-opt1, cov1 = curve_fit(func, x, y_int)
+y_non = y_non/V
+y_043 = y_043/V
+y_433 = y_433/V
 # fit the non-interactive case
-opt2, cov2 = curve_fit(func, x, y_non)
+opt1, cov1 = curve_fit(func, x, y_non)
+# fit the interactive cases
+opt2, cov2 = curve_fit(func, x, y_043)
+opt3, cov3 = curve_fit(func, x, y_433)
 
 # Plot
-plt.plot(x, func(x, *opt1), 'blue', label = "a = 0.433")
-plt.plot(x, func(x, *opt2), 'green', label = "a = 0.0")
+plt.plot(x, func(x, *opt1), 'mediumslateblue', label="a = 0.0")
+plt.plot(x, func(x, *opt2), 'darkorange', label="a = 0.0043")
+plt.plot(x, func(x, *opt3), 'yellowgreen', label="a = 0.433")
 
 plt.grid(color='gray', linestyle='-', linewidth=0.2)
 
 plt.xlabel(r" $r$")
-plt.ylabel(r"$\rho(r)$") 
+plt.ylabel(r"$\rho(r)$")
 # plt.xticks([0.1, 0.2, 0.3, 0.4])
 # plt.axis([1.0, 4.0, 0.0, 0.2])
 
